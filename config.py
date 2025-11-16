d48f45977c220fbe8c973c92268b3abf21d3d206
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-16 22:11:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ DS_STICKER = environ.get("DS_STICKER", "")
 DS_PIC = environ.get('DS_PIC', 'https://files.catbox.moe/6oh471.jpg')
 
 # Database Channel For Text Or Caption Store 
-DS_DESI_FILE_CHANNEL = int(environ.get("DS_DESI_FILE_CHANNEL", "-1002812150081"))
-DS_VIDESI_FILE_CHANNEL = int(environ.get("DS_VIDESI_FILE_CHANNEL", "-1002855994773"))
+DS_DESI_FILE_CHANNEL = int(environ.get("DS_DESI_FILE_CHANNEL", ""))
+DS_VIDESI_FILE_CHANNEL = int(environ.get("DS_VIDESI_FILE_CHANNEL", ""))
 FREE_LIMIT_DESI = 5
 FREE_LIMIT_VIDESI = 3
 PREMIUM_LIMIT_DESI = 40
