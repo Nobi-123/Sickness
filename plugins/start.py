bc59b674e5d937ceac2c4d2c9f02f6f1723c6144
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-22 23:33:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,7 +12,7 @@ from pyrogram.types import (
 from config import *
 from plugins.database import db          # Updated import
 from plugins.fsub import checkSub                # fsub.py is in root, so import directly
-from script import LOG_TEXT, ABOUT_TXT, DS_TEXT, DST_TEXT
+from plugins.script import LOG_TEXT, ABOUT_TXT, DS_TEXT, DST_TEXT
 from utils import check_and_increment
 
 
