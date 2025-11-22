2b3b3f3783fc58e16aee3957ace4a973c9e2ba96
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-22 22:47:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ DS_API_HASH = environ.get("DS_API_HASH", "231c18ea7273824491d6bf05425ab74e")
 DS_BOT_TOKEN = environ.get("DS_BOT_TOKEN", "")
 DS_BOT_USERNAME = environ.get("DS_BOT_USERNAME", "SicknessRoBot")  # bot username without @
 
-DS_LOG_CHANNEL = int(environ.get("DS_LOG_CHANNEL", "-1003373253670"))
+DS_LOG_CHANNEL = int(environ.get("DS_LOG_CHANNEL", "-1003496332303"))
 DS_STICKER = environ.get("DS_STICKER", "")
 DS_PIC = environ.get("DS_PIC", "https://files.catbox.moe/6oh471.jpg")
 
